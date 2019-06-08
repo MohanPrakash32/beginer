0c42a06dd70457b6f2a45c9aaf1033d5e2f9ef46
MohanPrakash32 <51431561+MohanPrakash32@users.noreply.github.com>
2019-06-08 14:23:34 +0530

    Create ply 5.py

---
+++ ply 5.py
@@ -0,0 +1,5 @@
+    
+inp =input()
+abc=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+if inp in abc:
+  print(abc.index(inp))
